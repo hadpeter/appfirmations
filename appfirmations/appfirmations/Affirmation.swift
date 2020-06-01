fbe96134b43936b88e2f16bb25b465c1dbfa375d
hadpeter <hadpeter@umich.edu>
2020-05-31 20:03:45 -0400

    saving fav affirmations and when click fav button a second time attempts to delte

--- appfirmations/appfirmations/Affirmation.swift
+++ appfirmations/appfirmations/Affirmation.swift
@@ -7,9 +7,20 @@
 //
 
 import UIKit
+import os.log
 
-class Affirmation: NSObject {
+class Affirmation: NSObject, NSCoding {
     // Mark Properties - static properties shared across all quotes
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    
+    struct PropertyKey {
+        static let text = "text"
+        static let author = "author"
+        static let fav = "fav"
+        static let font = "font"
+    }
+    
     var text: String
     var author: String
     var fav: Bool
@@ -28,5 +39,43 @@ class Affirmation: NSObject {
         self.author = author
         self.fav = fav
         self.font = font
+    }
+    
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(text, forKey: PropertyKey.text)
+        aCoder.encode(author, forKey: PropertyKey.author)
+        aCoder.encode(fav, forKey: PropertyKey.fav)
+        aCoder.encode(font, forKey: PropertyKey.font)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let text = aDecoder.decodeObject(forKey: PropertyKey.text) as? String else {
+            print("Unable to decode the text for an Affirmation object.")
+            return nil
+        }
+        
+        // Because photo is an optional property of Meal, just use conditional cast.
+        guard let author = aDecoder.decodeObject(forKey: PropertyKey.author) as? String else {
+            print("Unable to decode the author for an Affirmation object.")
+            return nil
+        }
+        
+        // decodeIntegerForKey unarchives an integer
+        guard let fav = aDecoder.decodeInteger(forKey: PropertyKey.fav) as? Bool else {
+            print("Unable to decode the fav for an Affirmation object.")
+            return nil
+        }
+        
+        guard let font = aDecoder.decodeObject(forKey: PropertyKey.font) as? String else {
+            print("Unable to decode the font for an Affirmation object.")
+            return nil
+        }
+        
+        // Must call designated initializer.
+        self.init(text: text, author: author, fav: fav, font: font)
+        
     }
 }
--- appfirmations/appfirmations/User.swift
+++ appfirmations/appfirmations/User.swift
@@ -5,6 +5,7 @@
 //  Created by Hadley Peterson on 5/27/20.
 
 import UIKit
+import os.log
 
 class User {
     var userName: String?
@@ -25,7 +26,26 @@ class User {
     }
     
     func updateFav() {
-        currentAffirmation.fav = true
-        favList.append(currentAffirmation)
+        if currentAffirmation.fav == true{
+            // want to unfav
+            currentAffirmation.fav = false
+            print("Need to implement deleting fav")
+        }
+        else {
+            currentAffirmation.fav = true
+            favList.append(currentAffirmation)
+            // need to savefav list
+            let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(favList, toFile: Affirmation.ArchiveURL.path)
+            if isSuccessfulSave {
+                os_log("Affirmation fav successfully saved.", log: OSLog.default, type: .debug)
+            } else {
+                os_log("Failed to save favorite...", log: OSLog.default, type: .error)
+            }
+        }
+    }
+    
+    // need to call this when pulling up fav affirmations
+    private func loadFavs() -> [Affirmation]?  {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Affirmation.ArchiveURL.path) as? [Affirmation]
     }
 }
