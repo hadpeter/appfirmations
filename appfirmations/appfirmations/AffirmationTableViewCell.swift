ea42fb21b60d0e0618f5d17eed8df27598284bfa
hadpeter <hadpeter@umich.edu>
2020-05-31 23:01:22 -0400

    table cell created but need to connect to code

---
+++ appfirmations/appfirmations/AffirmationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AffirmationTableViewCell.swift
+//  appfirmations
+//
+//  Created by Hadley Peterson on 5/31/20.
+//  Copyright © 2020 Hadley Peterson. All rights reserved.
+//
+
+import UIKit
+
+class AffirmationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
